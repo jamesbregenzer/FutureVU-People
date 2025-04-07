27710b23e772119b61203940df6acb469e886cf5
James Bregenzer <james@james.biz>
2025-04-08 02:34:21 +0530

    Enabled Timber/Twig functionality
    FutureVU theme using Timber WP theme which uses twig templates, enabled archive page to work with timber theme

--- archive-vu_people.php
+++ archive-vu_people.php
@@ -1,21 +1,68 @@
 <?php
-/**
- * @package WordPress
- * @subpackage vanderbilt brand
- */
+// Exit if accessed directly
+defined('ABSPATH') || exit;
 
-get_header();
+
+use Timber\Timber;
+use Timber\Site;
+
+if (!class_exists('Timber')) {
+    echo 'Timber not loaded';
+    exit;
+}
+
+$context = Timber::context();
+$context['theme_mods'] = get_theme_mods();
+$context['site'] = new \Timber\Site();
+$context['footer'] = 'default'; // or whatever your footer style is
+$context['bodyClass'] = 'custom-plugin-page';
+$context['vu_close_head_tag_section'] = ''; // or actual content
+$context['vu_close_body_tag_section'] = ''; // or actual content
+
+// Start output buffer
+ob_start();
 ?>
-<div class="panel panel-default col-sm-9">
-    <div class="row">
+<style type="text/css">
+    /**
+     * @todo move css to an asset fiele later
+     */
+    .container{
+        max-width: 85rem;
+        margin: auto;
+    }
+    .people-row{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 20px;
+    }
+    .people-row .people-photo img{
+        max-width: 100%;
+    }
+    .people-row .people-swatch{
+        width: 23%;
+    }
+    .people-row .people-stats{
+        background: #EEE;
+        padding: 10px;
+    }
+    @media(max-width: 980px){
+        .people-row .people-swatch{
+            width: 30%;
+        }
+    }
+    @media(max-width: 600px){
+        .people-row .people-swatch{
+            width: 45%;
+        }
+    }
+</style>
+<div class="container">
         <article class="primary-content col-sm-12">
             <div class="panel-body">
-                <?php $blog_details = get_blog_details(); 
-
-                ?>
-                <h2> <?php echo _($blog_details->blogname); ?> Members </h2>
+                <?php $blog_details = get_blog_details(); ?>
+                <h2> <?php echo esc_html($blog_details->blogname); ?> Members </h2>
                 <?php
-
                 $args = array(
                     'post_type' => 'person',
                     'depth' => 1,
@@ -25,68 +72,61 @@ get_header();
                     'orderby' => 'meta_value',
                     'order' => 'ASC',
                 );
-
                 $wp_query = new WP_Query($args);
                 ?>
-                <div class="row">
+                <div class="people-row">
                     <?php if($wp_query->have_posts()):
                         while($wp_query->have_posts()):
-
                             $wp_query->the_post();
-                            ?>
-
-                            <div class="col-xs-6 col-sm-4 col-md-3 people-swatch">
-                                <div class="people-photo">
+                    ?>
+                        <div class="col-xs-6 col-sm-4 col-md-3 people-swatch">
+                            <div class="people-photo">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php if (has_post_thumbnail()): ?>
+                                        <?php the_post_thumbnail('img-142-190-grid'); ?>
+                                    <?php else: ?>
+                                        <img class="media-object img-thumbnail" src="<?php echo plugins_url('images/default-person-pic-150.png', __FILE__); ?>" />
+                                    <?php endif; ?>
+                                </a>
+                            </div>
+                            <div class="people-stats">
+                                <h4 class="people-name">
                                     <a href="<?php the_permalink(); ?>">
-                                        <?php if (get_the_post_thumbnail()): ?>
-                                            <?php the_post_thumbnail('img-142-190-grid'); ?>
-                                        <?php else: ?>
-                                            <img class="media-object img-thumbnail" src="<?php echo plugins_url('images/default-person-pic-150.png', __FILE__); ?>" />
-                                        <?php endif;?>
+                                        <?php the_field('first_given_name'); ?> <?php the_field('middle_name_or_initial'); ?> <?php the_field('last_family_name'); ?>
                                     </a>
-                                </div>
-                                <div class="people-stats">
-                                    <h4 class="people-name">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php the_field('first_given_name'); ?> <?php the_field('middle_name_or_initial'); ?> <?php the_field('last_family_name'); ?>
-                                        </a>
-                                    </h4>
-                                    <div class="people-title">
-
-                                        <?php if(have_rows('title_and_department')): ?>
-                                            <?php while(have_rows('title_and_department')): the_row(); ?>
-                                                <?php if(get_sub_field('title__position')): ?>
-                                                    <span class="people-title-position"><?php the_sub_field('title__position'); ?></span>
-                                                        <?php if(get_sub_field('department__center__office')): ?>
-                                                        , <span class="people-title-department"><?php the_sub_field('department__center__office'); ?></span>
-                                                        <?php endif; ?>
-                                                     <br />
-                                                    <?php
-                                                        // break here because we don't want to list out all the title in a small space
-                                                        break;
-                                                    ?>
+                                </h4>
+                                <div class="people-title">
+                                    <?php if(have_rows('title_and_department')): ?>
+                                        <?php while(have_rows('title_and_department')): the_row(); ?>
+                                            <?php if(get_sub_field('title__position')): ?>
+                                                <span class="people-title-position"><?php the_sub_field('title__position'); ?></span>
+                                                <?php if(get_sub_field('department__center__office')): ?>
+                                                    , <span class="people-title-department"><?php the_sub_field('department__center__office'); ?></span>
                                                 <?php endif; ?>
-                                            <?php endwhile; ?>
-                                        <?php endif; ?>
-
-                                    </div>
-                                    <div class="people-email">
-                                        <?php if(get_field('email')): ?>
-                                            <a href="mailto:<?php the_field('email'); ?>"> <i class="fa fa-envelope" aria-hidden="true"></i> <?php the_field('email'); ?> </a>
-                                        <?php endif; ?>
-                                    </div>
+                                                <br />
+                                                <?php break; ?>
+                                            <?php endif; ?>
+                                        <?php endwhile; ?>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="people-email">
+                                    <?php if(get_field('email')): ?>
+                                        <a href="mailto:<?php the_field('email'); ?>">
+                                            <i class="fa fa-envelope" aria-hidden="true"></i> <?php the_field('email'); ?>
+                                        </a>
+                                    <?php endif; ?>
                                 </div>
                             </div>
-                        <?php endwhile;
-                    endif;
-                    ?>
+                        </div>
+                    <?php endwhile; endif; ?>
                 </div>
-
-                <?php wp_reset_query(); ?>
-            </div>
+                <?php wp_reset_postdata(); ?>
         </article>
     </div>
 </div>
+<?php
+// Store output buffer
+$context['plugin_content'] = ob_get_clean();
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+// Render your twig template that extends the theme
+Timber::render('plugin-page.twig', $context);
